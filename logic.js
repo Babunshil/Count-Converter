8f016c709dc4cc9eca9cceff42c7a16bd691d32b
Babun Shil <shilbabun491@gmail.com>
2023-09-18 20:32:40 +0530

    fixed for small device

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 export function logic(system, number) {
 
   if (system === 'tex') {
-    return [(number * 9.100).toFixed(3), number];
+    return [(number * 9.100).toFixed(3), number, (number * 0.028).toFixed(3), (number * 590).toFixed(3), (number * 1940).toFixed(3), (number * 1650).toFixed(3), (number * 886).toFixed(3), (number * 3100).toFixed(3), (number * 10).toFixed(3), (number * 1000).toFixed(3)];
   }
   if (system === 'denier') {
     return [number, number * 0.1];
@@ -14,3 +14,5 @@ export function logic(system, number) {
 export function systemValue() {
   return (['Tex', 'Denier', 'Grist', 'English', 'Woollen', 'Linen', 'Worsted', 'Dewsbury', 'Decitex', 'Metric',])
 }
+
+// , (number * 0.028).toFixed(3), (number * 590).toFixed(3), (number * 1940).toFixed(3), (number * 1650).toFixed(3), (number * 886).toFixed(3), (number * 3100).toFixed(3), (number * 10).toFixed(3), (number * 1000).toFixed(3)
